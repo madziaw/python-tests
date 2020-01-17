eea24ee8966cbd1850e511ad8c60d516ae6a3455
Magdalena Wasilewska <magdalena.barbara.wasilewska@gmail.com>
2020-01-17 10:17:18 +0100

    Add new file

---
+++ test_group_and_count.py
@@ -0,0 +1,10 @@
+import pytest
+import group_and_count
+import datetime
+
+
+def test_group_and_count_returns_correct_result():
+    collection = [(datetime.date(2019,12, 29), 10),(datetime.date(2019,12,30), 15), (datetime.date(2019,12,31), 20), (datetime.date(2020,1,1), 25), (datetime.date(2020,1,2), 30)]
+    expected_result = {(2019, 12): 45, (2020, 1): 55}
+    result = group_and_count.group_and_count(collection)    
+    assert expected_result == result
